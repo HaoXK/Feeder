10a97c9bfa8af8b793a4fe2a12578c14d97b8dc6
HaoXK <haoxuank@nycstudents.net>
2024-09-23 12:36:08 -0400

    Update Feeder.java

--- Feeder.java
+++ Feeder.java
@@ -10,8 +10,15 @@ public class Feeder {
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
+    public void simulateOneDay(int numBirds) 
     {
+        if (Math.random() < 0.05) currentFood = 0;
+        else 
+        {
+            int eaten = (int)(Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if (currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
@@ -32,4 +39,6 @@ public class Feeder {
         currentFood = c;
     }
     public Feeder() {}
+    public String toString()
+    
 }
